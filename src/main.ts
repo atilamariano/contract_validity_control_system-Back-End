d2f27619ad6f6f1ebb6483d43baa395ecb0b2c97
atilamariano <atilamariano@hotmail.com>
2023-06-29 16:28:14 -0300

    fix: erro req in deploy

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Documentação com Swagger - Listagem de')
@@ -13,7 +12,7 @@ async function bootstrap() {
       'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
     )
     .setVersion('1.0')
-    .addServer('http://localhost:3333')
+    .addServer('https://eliteconsultores-backend-production.up.railway.app/')
     .addTag('Company')
     .build();
 
