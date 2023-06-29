d8a1c7a7636788907196331aaf1dee9262ab6697
atilamariano <atilamariano@hotmail.com>
2023-06-29 15:48:56 -0300

    fix: resolved error deploy

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,13 @@ async function bootstrap() {
       'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
     )
     .setVersion('1.0')
+    .addServer('http://localhost:3333')
     .addTag('Company')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
