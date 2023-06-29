6d6e74e97dfa53b68b900be4a56077adc33b522b
atilamariano <atilamariano@hotmail.com>
2023-06-29 09:50:12 -0300

    feat: install Swagger end implemente doc from the API

--- src/company/dto/update-company.dto.ts
+++ src/company/dto/update-company.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCompanyDto } from './create-company.dto';
+import { EnumStatus } from '@prisma/client';
 
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
+export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
+  code?: string;
+  cnpj?: string;
+  businessName?: string;
+  contactPerson?: string;
+  contactPhone?: string;
+  contactEmail?: string;
+  inclusionDtae?: Date;
+  status?: EnumStatus;
+  municipalRegistration?: string;
+}
